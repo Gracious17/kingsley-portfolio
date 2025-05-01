ba26429c9b20986e6714f1122c9d520558010f76
Gracious-kingsley <Kingsleygracious16@gmail.com>
2025-05-01 15:21:33 +0100

    added more details to project

--- app/hotelBooking/page.tsx
+++ app/hotelBooking/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import hotelImg from "@/public/hotel-booking.png";
 import { RiRadioButtonFill } from "react-icons/ri";
+import { IoRadioButtonOnOutline } from "react-icons/io5";
 import Video3 from "../components/Video3";
 import Link from "next/link";
 const HotelBooking = () => {
@@ -11,7 +12,7 @@ const HotelBooking = () => {
     setMyvid(!Myvid);
   };
   return (
-    <div className="w-full">
+    <div className="w-full overflow-hidden">
       <div className="w-screen h-[30vh] lg:h-[40vh] relative">
         {/* overlay */}
         <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
@@ -33,10 +34,9 @@ const HotelBooking = () => {
           <p>Project</p>
           <h2>Overview</h2>
           <p>
-            This app was buit using Next Js , Users are able to // Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Adipisci ut nostrum
-            veritatis quisquam at debitis commodi iste inventore. Quos,
-            laboriosam?
+            A fully responsive hotel booking application built with Next.js 15,
+            TypeScript, and Tailwind CSS, designed to streamline the hotel
+            discovery and reservation process.
           </p>
           {/* <a
             href=""
@@ -44,7 +44,7 @@ const HotelBooking = () => {
             rel="noopener noreferrer"
             aria-label="Open Demo"
           > */}
-            <button className="px-8 py-2 mt-4 mr-4">Demo</button>
+          <button className="px-8 py-2 mt-4 mr-4">Demo</button>
           {/* </a> */}
 
           <button className="px-8 py-2 mt-4">Code</button>
@@ -80,6 +80,12 @@ const HotelBooking = () => {
               <p className="text-gray-600 py-2 flex items-center ">
                 <RiRadioButtonFill className="pr-1" /> NextAuth
               </p>
+              <p className="text-gray-600 py-2 flex items-center ">
+                <RiRadioButtonFill className="pr-1" /> Zustand
+              </p>
+              <p className="text-gray-600 py-2 flex items-center ">
+                <RiRadioButtonFill className="pr-1" /> Paystack
+              </p>
 
               <p className="text-gray-600 py-2 flex items-center ">
                 <RiRadioButtonFill className="pr-1" /> Google api
@@ -87,6 +93,106 @@ const HotelBooking = () => {
             </div>
           </div>
         </div>
+      </div>
+      {/* more details section */}
+      <div className="w-full py-4 ml-4 gap-3 px-2">
+        <hr />
+        <h2>Idea-To-Impact</h2>
+
+        <details>
+          <summary className="font-bold text-black text-xl tracking-wide"> Problem</summary>
+          <h3 className="text-sm font-semibold">
+            Hotel search platforms often lack clarity, real-time availability,
+            and seamless payment flow, especially in growing markets. Users find
+            it difficult to:
+          </h3>
+          <ul>
+            <li className="text-gray-600 py-2 flex items-center ">
+              {" "}
+              <IoRadioButtonOnOutline className="pr-1" />
+              Filter hotels based on key preferences
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              View accurate, up-to-date availability
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Pay securely and receive booking confirmation
+            </li>
+          </ul>
+        </details>
+
+        <details>
+          <summary className="font-bold text-black text-xl tracking-wide"> Solution</summary>
+          <h3 className="text-sm font-semibold">
+            Built a multi-page booking flow with the following features: :
+          </h3>
+          <ul>
+            <li className="text-gray-600 py-2 flex items-center">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Hotel Listing Page: Shows dynamically fetched hotels with filters
+              (price, rating, distance)
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Hotel Details Page: Displays selected hotel&apos;s info and room
+              availability
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Pay securely and receive booking confirmation
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Payment Page: Accepts guest info and initiates payment through
+              Paystack
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Confirmation Page: Displays success message, booking ID, and sends
+              email confirmation
+            </li>
+          </ul>
+        </details>
+
+        <details>
+          <summary className="font-bold text-black text-xl tracking-wide">Challenges</summary>
+          <ul>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Ensuring smooth client-server state sync across pages
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Handling Paystack redirection and transaction verification
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Making the UI accessible and mobile-first
+            </li>
+          </ul>
+        </details>
+        <details>
+          <summary className="font-bold text-black text-xl tracking-wide">Results</summary>
+          <ul>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Completed a responsive and test-ready booking flow.
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Reduced booking steps to just 2 screens.
+            </li>
+            <li className="text-gray-600 py-2 flex items-center ">
+              <IoRadioButtonOnOutline className="pr-1" />
+              Modular and API-integrated components allow for easy scaling.
+            </li>
+          </ul>
+        </details>
+      </div>
+      {/* go back  btn */}
+      <div className="w-full p-4">
         <Link href="/#projects">
           <p className="underline cursor-pointer">Back</p>
         </Link>
